b55b5e607942a4e1aff33b73edcb91cb37e85a79
jaredhall18 <jaredhall18@gmail.com>
2021-12-10 11:10:21 -0700

    installed dependencies and set up server and index.js

--- api/server.js
+++ api/server.js
@@ -1,1 +1,17 @@
 // build your server here and require it from index.js
+const express = require("express");
+const server = express();
+
+
+
+
+//error handling mdlware
+server.use((err, req, res, next) => {
+    // eslint-disable-line
+    res.status(500).json({
+      message: err.message,
+      stack: err.stack,
+    });
+  });
+  
+  module.exports = server;
