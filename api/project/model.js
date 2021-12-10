6c932d0d9d8682ae39f8197efc1c289188df4b0a
jaredhall18 <jaredhall18@gmail.com>
2021-12-10 14:47:26 -0700

    get projects working

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,15 @@
 // build your `Project` model here
+const db = require('../../data/dbConfig')
+
+async function getProjects() {
+    const rows = await db('projects')
+    
+   return rows.map((resp) => ({
+        ...resp,
+        project_completed: !!resp.project_completed
+    }))
+}
+
+module.exports = {
+    getProjects,
+}
--- api/project/router.js
+++ api/project/router.js
@@ -1,6 +1,15 @@
 // build your `/api/projects` router here
 const express = require('express');
+const Projects = require('./model')
 
 const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    Projects.getProjects()
+    .then(projects => {
+        res.status(200).json(projects)
+    })
+    .catch(next)
+})
 
 module.exports = router;
