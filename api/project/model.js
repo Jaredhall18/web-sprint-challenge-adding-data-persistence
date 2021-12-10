1e30eb7f7a54aa1119b4864b5a970dca0d9b60b3
jaredhall18 <jaredhall18@gmail.com>
2021-12-10 15:04:02 -0700

    project get working

--- api/project/model.js
+++ api/project/model.js
@@ -10,6 +10,19 @@ async function getProjects() {
     }))
 }
 
+async function createProject(project) {
+   return db('projects').insert(project)
+    .then(([project_id]) => {
+        return db('projects').where('project_id', project_id)
+            .first()
+            .then((resp) => ({
+                ...resp,
+                project_completed: !!resp.project_completed
+            })) 
+    })
+}
+
 module.exports = {
     getProjects,
+    createProject,
 }
--- api/project/router.js
+++ api/project/router.js
@@ -12,4 +12,12 @@ router.get('/', (req, res, next) => {
     .catch(next)
 })
 
+router.post("/", (req, res, next) => {
+    Projects.createProject(req.body)
+      .then((task) => {
+        res.status(201).json(task);
+      })
+      .catch(next);
+  });
+
 module.exports = router;
