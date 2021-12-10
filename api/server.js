5b45f50adaf920c43570737e65a2b7366bb5b3e3
jaredhall18 <jaredhall18@gmail.com>
2021-12-10 13:34:58 -0700

    server file importing routes.

--- api/server.js
+++ api/server.js
@@ -1,9 +1,19 @@
 // build your server here and require it from index.js
 const express = require("express");
 const server = express();
+const resourceRouter = require('./resource/router')
+const projectRouter = require('./project/router')
+const taskRouter = require('./task/router')
 
+server.use(express.json());
+server.use('/api/projects', projectRouter)
+server.use('/api/tasks', taskRouter)
+server.use('/api/resources', resourceRouter)
 
-
+//Test endpoint
+server.use('*', (req, res) => {
+    res.json({ api: 'up'})
+  })
 
 //error handling mdlware
 server.use((err, req, res, next) => {
